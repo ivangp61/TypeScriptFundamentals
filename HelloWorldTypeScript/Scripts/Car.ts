879c92875cd0066a3202e77730d072c6365ba5df
Ivan Gonzalez Perez <ivangp61@gmail.com>
2018-09-11 11:20:30 -0400

    Typescript Fundamentals Course
    Created HelloWorld project for the pluralsight' s typescript fundamentals course.

---
+++ HelloWorldTypeScript/Scripts/Car.ts
@@ -0,0 +1,21 @@
+﻿class Car {
+    engine: string;
+
+    constructor(engine: string) {
+        this.engine = engine;
+    }
+
+    start() {
+        alert('Engine Started ' + this.engine);
+    }
+
+    stop() {
+        alert('Engine Stopped: ' + this.engine);
+    }    
+}
+
+window.onload = function () {
+    var car = new Car('V8');
+    car.start();
+    car.stop();
+};
